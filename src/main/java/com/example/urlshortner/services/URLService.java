54d1591b30abc948f4ab8a10019f1cd1dfcf9bbf
Prashant <prashant.triphathi@ssipmt.com>
2026-05-30 00:20:53 +0530

    added the logger in URLService class

--- src/main/java/com/example/urlshortner/services/URLService.java
+++ src/main/java/com/example/urlshortner/services/URLService.java
@@ -2,24 +2,30 @@ package com.example.urlshortner.services;
 
 import com.example.urlshortner.entities.URLEntity;
 import com.example.urlshortner.repositories.URLTable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+
 @Service
 public class URLService {
 
+    public static final Logger logger = LoggerFactory.getLogger(URLService.class);
     @Autowired
     URLTable urlTable;
 
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     public String encode(String url){
+        logger.info("Start encoding logn url: {}",url);
         URLEntity urlEntity = new URLEntity();
 
         urlEntity.setUrl(url);
 
         urlEntity = urlTable.save(urlEntity);
+        logger.info("Saved the long url in db url: {}",url);
         long id = urlEntity.getId();
 
         StringBuilder shortUrl = new StringBuilder();
@@ -29,12 +35,14 @@ public class URLService {
              id/=62;
              shortUrl.append(CHARACTERS.charAt(r));
         }while(id != 0);
-        return shortUrl.reverse().toString();
+        String shortURL = shortUrl.reverse().toString();
+        logger.info("Short url genrated url: {}",shortURL);
+        return shortURL;
     }
 
     public String decode(String shortURL){
         long id = 0;
-
+        logger.info("Start decoidng the short url: {}",shortURL);
         for(char c : shortURL.toCharArray()){
             int index = CHARACTERS.indexOf(c);
             if(index == -1){
@@ -42,8 +50,11 @@ public class URLService {
             }
             id = id*62+index;
         }
+        logger.info("DB id genreated from short url id: {}",id);
         Optional<URLEntity> urlEntity = urlTable.findById(id);
-        return  urlEntity.isPresent()?urlEntity.get().getUrl()
+        String longurl = urlEntity.isPresent()?urlEntity.get().getUrl()
                 :"";
+        logger.info("Long url found in DB url: {}",longurl);
+        return  longurl;
     }
 }
